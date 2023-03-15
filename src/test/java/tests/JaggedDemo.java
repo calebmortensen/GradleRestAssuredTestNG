d7fed38b14bb9f10827d49e16e121812748f538b
calebmortensen <caleb.mortensen@live.com>
2023-03-15 13:22:23 -0700

    Jagged Array

---
+++ src/test/java/tests/JaggedDemo.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class JaggedDemo {
+
+	
+
+	@Test(dataProvider="supplier")
+	public void sampleTest(Object[] a) {
+		
+		for (int i=0; i<a.length;i++) {
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+		System.out.println("----------");
+	}
+	
+	@DataProvider(name="supplier")
+	public Object[][] dataSupplier() {
+		//JAGGED 2 Dimensional array with different value counts
+		Object[][] data = {
+				{"Caleb", "Mortensen", "is", "awesome"},
+				{"Joe", "Blow"},
+				{"John", "Doe","seven", "eight", "nine", "ten"}};
+		
+		return data;
+	}
+	
+	
+}
